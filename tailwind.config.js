eefa678d8208a98f94c19bdbe2d6764a7bb8875c
Ivan Ditchon <ivnditchon@gmail.com>
2022-03-03 11:29:30 +0800

    Creating animations and keyframes object for skills graphs

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,65 @@ module.exports = {
             backgroundImage: theme => ({
                 'skills': 'url("../images/skills.jpg")',
                 'experience': 'url("../images/experience.jpg")'
-            })
+            }),
+            animation: {
+                'skills-graph-html': 'graphHTML 2s ease-in-out 0.5s forwards',
+                'skills-graph-css': 'graphCSS 2s ease-in-out 1s forwards',
+                'skills-graph-javascript': 'graphJavascript 3s ease-in-out 1.5s forwards',
+                'skills-graph-react': 'graphReact 2s ease-in-out 2s forwards',
+                'skills-graph-figma': 'graphFigma 2s ease-in-out 2.5s forwards',
+                'skills-graph-adobe': 'graphAdobe 2s ease-in-out 3s forwards'
+            },
+            keyframes: {
+                graphHTML: {
+                    '0%': {
+                        width: '0%',
+                    },
+                    '100%': {
+                        width: '90%'
+                    }
+                },
+                graphCSS: {
+                    '0%': {
+                        width: '0%',
+                    },
+                    '100%': {
+                        width: '80%'
+                    }
+                },
+                graphJavascript: {
+                    '0%': {
+                        width: '0%',
+                    },
+                    '100%': {
+                        width: '60%'
+                    }
+                },
+                graphReact: {
+                    '0%': {
+                        width: '0%',
+                    },
+                    '100%': {
+                        width: '20%'
+                    }
+                },
+                graphFigma: {
+                    '0%': {
+                        width: '0%',
+                    },
+                    '100%': {
+                        width: '30%'
+                    }
+                },
+                graphAdobe: {
+                    '0%': {
+                        width: '0%',
+                    },
+                    '100%': {
+                        width: '30%'
+                    }
+                }
+            }
         },
     },
     plugins: [],
