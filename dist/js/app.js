9d8fea067fc5427e4b3ef5279d7645437defaf02
Ivan Ditchon <ivnditchon@gmail.com>
2022-03-05 15:11:54 +0800

    Creating function for dark mode

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,54 +3,28 @@ const html = document.querySelector('html');
 const topBar = document.querySelector('#top_bar');
 const topBarMenuIcon = document.querySelector('#topbar_menu_icon');
 const topBarDarkmodeIcon = document.querySelector('#topbar_darkmode_icon');
-const htmlBarGraph = document.querySelector('#html_bar_graph');
-const cssBarGraph = document.querySelector('#css_bar_graph');
-const javascriptBarGraph = document.querySelector('#javascript_bar_graph');
-const reactBarGraph = document.querySelector('#react_bar_graph');
-const figmaBarGraph = document.querySelector('#figma_bar_graph');
-const adobeBarGraph = document.querySelector('#adobe_bar_graph');
-const darkModeBtn = document.querySelector('#dark_mode_btn');
+const navDarkModeBtn = document.querySelector('#nav_dark_mode_btn');
+const mobileDarkModeBtn = document.querySelector('#mobile_dark_mode_btn');
 
-// Scroll event
-const scroll = () => {
-    window.addEventListener('scroll', () => {
-        const scrolled = window.scrollY;
+// Darkmode toggle
+const darkModeToggle = () => {
+    navDarkModeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
 
-        // Remove color icons and background of topbar when scroll y is less than 100 
-        if (scrolled >= 100) {
-            if (!topBar.classList.contains('bg-primary') && topBarDarkmodeIcon.classList.contains('text-gray-900') && topBarMenuIcon.classList.contains('text-gray-900')) {
-                topBarDarkmodeIcon.classList.remove('text-gray-900');
-                topBarMenuIcon.classList.remove('text-gray-900');
-                topBar.classList.add('bg-primary'); // Update background color
-                // Update color icons 
-                topBarDarkmodeIcon.classList.add('text-gray-200');
-                topBarMenuIcon.classList.add('text-gray-200');
-            }
-            // Remove color icons and background of topbar when scroll y is less than 100 
+        if (localStorage.theme === 'dark') {
+            html.classList.remove('dark');
+            localStorage.removeItem('theme');
+            topBarDarkmodeIcon.classList.add('text-gray-900');
+            topBarDarkmodeIcon.classList.remove('text-gray-200');
         } else {
-            if (topBar.classList.contains('bg-primary') && topBarDarkmodeIcon.classList.contains('text-gray-200') && topBarMenuIcon.classList.contains('text-gray-200')) {
-                topBar.classList.remove('bg-primary');
-                topBarDarkmodeIcon.classList.remove('text-gray-200');
-                topBarMenuIcon.classList.remove('text-gray-200');
-                topBarDarkmodeIcon.classList.add('text-gray-900');
-                topBarMenuIcon.classList.add('text-gray-900');
-            }
-        }
-
-        if (scrolled >= 915) {
-            // Animate bar graph 
-            htmlBarGraph.classList.add('animate-skills-graph-html'); // width to 90%
-            cssBarGraph.classList.add('animate-skills-graph-css'); // width to 80%
-            javascriptBarGraph.classList.add('animate-skills-graph-javascript'); // width to 60%
-            reactBarGraph.classList.add('animate-skills-graph-javascript'); // width to 20%
-            figmaBarGraph.classList.add('animate-skills-graph-figma'); // width to 30%
-            adobeBarGraph.classList.add('animate-skills-graph-adobe'); // width to 30%
+            html.classList.add('dark');
+            localStorage.theme = 'dark';
+            topBarDarkmodeIcon.classList.remove('text-gray-900');
+            topBarDarkmodeIcon.classList.add('text-gray-200');
         }
     });
-}
 
-const darkModeToggle = () => {
-    darkModeBtn.addEventListener('click', (e) => {
+    mobileDarkModeBtn.addEventListener('click', (e) => {
         e.preventDefault();
 
         if (localStorage.theme === 'dark') {
