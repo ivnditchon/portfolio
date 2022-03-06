da4b84d6980b5dd5a10262f513916855a7e27192
Ivan Ditchon <ivnditchon@gmail.com>
2022-03-07 00:35:40 +0800

    Creating function for view all skills and projects

--- dist/js/app.js
+++ dist/js/app.js
@@ -2,6 +2,10 @@
 const html = document.querySelector('html');
 const mobileDarkModeBtn = document.querySelector('#mobile_dark_mode_btn');
 const mobileDarkModeIcon = document.querySelector('#mobile_dark_mode_icon');
+const viewSkillsBtn = document.querySelector('#view_skills_btn');
+const hiddenSkills = document.querySelectorAll('#hidden_skill');
+const viewProjectsBtn = document.querySelector('#view_projects_btn');
+const hiddenProjects = document.querySelectorAll('#hidden_project');
 
 // Darkmode toggle
 const darkModeToggle = () => {
@@ -21,6 +25,7 @@ const darkModeToggle = () => {
     });
 }
 
+// Check darkmode theme in localstorage if existing
 const darkMode = () => {
     if (localStorage.theme === 'dark') {
         html.classList.add('dark');
@@ -31,12 +36,50 @@ const darkMode = () => {
     }
 }
 
+// Show all skills
+const showAllSkills = () => {
+    viewSkillsBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        Array.from(hiddenSkills, (skill) => {
+            if (skill.classList.contains('hidden')) {
+                skill.classList.remove('hidden');
+                skill.classList.remove('opacity-0');
+                skill.classList.add('flex');
+            } else {
+                if (skill.classList.contains('flex')) {
+                    skill.classList.remove('flex');
+                    skill.classList.add('hidden');
+                    skill.classList.add('opacity-0');
+                }
+            }
+        });
+    });
+}
+
+// Show all projects
+const showAllProjects = () => {
+    viewProjectsBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        Array.from(hiddenProjects, (project) => {
+            if (project.classList.contains('hidden', 'opacity-0')) {
+                project.classList.remove('hidden', 'opacity-0');
+            } else {
+                project.classList.add('hidden', 'opacity-0');
+            }
+        });
+    });
+}
+
 const main = () => {
     scroll();
     darkModeToggle();
     document.addEventListener('DOMContentLoaded', () => {
         darkMode();
-    })
+    });
+    showAllSkills();
+    showAllProjects();
 }
 
 main();
