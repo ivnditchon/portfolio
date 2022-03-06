ac83f16f32a039d4797864dbc093ec5989cede50
Ivan Ditchon <ivnditchon@gmail.com>
2022-03-06 19:41:55 +0800

    Creating function for darkmode

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,42 +1,22 @@
 // DOM Elements
 const html = document.querySelector('html');
-const topBar = document.querySelector('#top_bar');
-const topBarMenuIcon = document.querySelector('#topbar_menu_icon');
-const topBarDarkmodeIcon = document.querySelector('#topbar_darkmode_icon');
-const navDarkModeBtn = document.querySelector('#nav_dark_mode_btn');
 const mobileDarkModeBtn = document.querySelector('#mobile_dark_mode_btn');
+const mobileDarkModeIcon = document.querySelector('#mobile_dark_mode_icon');
 
 // Darkmode toggle
 const darkModeToggle = () => {
-    navDarkModeBtn.addEventListener('click', (e) => {
+    mobileDarkModeIcon.addEventListener('click', (e) => {
         e.preventDefault();
 
         if (localStorage.theme === 'dark') {
             html.classList.remove('dark');
             localStorage.removeItem('theme');
-            topBarDarkmodeIcon.classList.add('text-gray-900');
-            topBarDarkmodeIcon.classList.remove('text-gray-200');
+            mobileDarkModeIcon.src = './images/moon.svg';
+
         } else {
             html.classList.add('dark');
             localStorage.theme = 'dark';
-            topBarDarkmodeIcon.classList.remove('text-gray-900');
-            topBarDarkmodeIcon.classList.add('text-gray-200');
-        }
-    });
-
-    mobileDarkModeBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-
-        if (localStorage.theme === 'dark') {
-            html.classList.remove('dark');
-            localStorage.removeItem('theme');
-            topBarDarkmodeIcon.classList.add('text-gray-900');
-            topBarDarkmodeIcon.classList.remove('text-gray-200');
-        } else {
-            html.classList.add('dark');
-            localStorage.theme = 'dark';
-            topBarDarkmodeIcon.classList.remove('text-gray-900');
-            topBarDarkmodeIcon.classList.add('text-gray-200');
+            mobileDarkModeIcon.src = './images/sun.svg';
         }
     });
 }
@@ -44,8 +24,10 @@ const darkModeToggle = () => {
 const darkMode = () => {
     if (localStorage.theme === 'dark') {
         html.classList.add('dark');
+        mobileDarkModeIcon.src = './images/sun.svg';
     } else {
         html.classList.remove('dark');
+        mobileDarkModeIcon.src = './images/moon.svg';
     }
 }
 
