cb5e62b5d9722d7ea1707f941ee81a87d12a0720
Ivan Ditchon <ivnditchon@gmail.com>
2022-03-07 11:32:32 +0800

    Uninstall scroll bar hide

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,12 +38,6 @@ module.exports = {
                 'experience': 'url("../images/experience.jpg")'
             }),
             animation: {
-                /*'skills-graph-html': 'graphHTML 2s ease-in-out 0.5s forwards',
-                'skills-graph-css': 'graphCSS 2s ease-in-out 1s forwards',
-                'skills-graph-javascript': 'graphJavascript 3s ease-in-out 1.5s forwards',
-                'skills-graph-react': 'graphReact 2s ease-in-out 2s forwards',
-                'skills-graph-figma': 'graphFigma 2s ease-in-out 2.5s forwards',
-                'skills-graph-adobe': 'graphAdobe 2s ease-in-out 3s forwards',*/
                 'dark-mode': 'darkMode 0.4s ease-in-out forwards',
                 'skills': 'skills 0.3s linear forwards'
             },
@@ -56,54 +50,6 @@ module.exports = {
                         transform: 'scale(1.1)'
                     }
                 },
-                /*graphHTML: {
-                    '0%': {
-                        width: '0%',
-                    },
-                    '100%': {
-                        width: '90%'
-                    }
-                },
-                graphCSS: {
-                    '0%': {
-                        width: '0%',
-                    },
-                    '100%': {
-                        width: '80%'
-                    }
-                },
-                graphJavascript: {
-                    '0%': {
-                        width: '0%',
-                    },
-                    '100%': {
-                        width: '60%'
-                    }
-                },
-                graphReact: {
-                    '0%': {
-                        width: '0%',
-                    },
-                    '100%': {
-                        width: '20%'
-                    }
-                },
-                graphFigma: {
-                    '0%': {
-                        width: '0%',
-                    },
-                    '100%': {
-                        width: '30%'
-                    }
-                },
-                graphAdobe: {
-                    '0%': {
-                        width: '0%',
-                    },
-                    '100%': {
-                        width: '30%'
-                    }
-                },*/
                 darkMode: {
                     'from': {
                         transform: 'translate(0%, -50%)'
